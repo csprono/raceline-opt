c0f006d84a6971613adb8538baecb76e47438656
Adam Keating <adamkeating@xtra.co.nz>
2023-04-15 09:08:27 +1200

    WIP: Device Transfer

--- testing/spline creation.py
+++ testing/spline creation.py
@@ -1,10 +1,26 @@
 import math
 import numpy as np
 from scipy import interpolate
-import trajectory_planning_helpers as tph
+import matplotlib.pyplot as plt
 
-track = np.loadtxt(r'D:\Code\racing line optimiser\src\centreline_gen\output\centre_line.csv',delimiter=',')
+track = np.loadtxt(r'src\centreline_gen\output\centre_line.csv',delimiter=',')
+
+# sorted_indices = np.argsort(track[:, 0])
+# track = track[sorted_indices]
 
 
-t = 1:3
+# track = np.vstack([track, track[0]])
 
+
+
+x = track [:100:,0]
+y = track[:100:,1]
+
+tck,u = interpolate.splprep([x,y], s=0,)
+xi, yi = interpolate.splev(np.linspace(0, 1, 1000), tck)
+
+# plot the result
+fig, ax = plt.subplots(1, 1)
+ax.plot(x, y, 'or')
+ax.plot(xi, yi, '-b')
+plt.show()
