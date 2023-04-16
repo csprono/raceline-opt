4525196e5d5bc2d005c72018313eca4c29c6fcab
Adam Keating <adamkeating@xtra.co.nz>
2023-04-16 13:35:41 +1200

    WIP

--- testing/tangent test.py
+++ testing/tangent test.py
@@ -8,10 +8,10 @@ from matplotlib.collections import LineCollection
 def create_splines(centreline:np.ndarray):
     centreline = np.vstack([centreline, centreline[0]])
     
-    x = centreline[:100:,0]
-    y = centreline[:100:,1]
+    x = centreline[::,0]
+    y = centreline[::,1]
     
-    spline_tck, u = interpolate.splprep([x,y], s=300, per=True) 
+    spline_tck, u = interpolate.splprep([x,y], s=300) 
     
     #evaluate spline at given point
     xi, yi = interpolate.splev(u, spline_tck)
@@ -28,14 +28,14 @@ def calc_tangent(splinerep_tck:tuple, u, x_vals, y_vals):
     y_ints = y_vals - tan_slopes * x_vals #array of y_ints for tangents at each point
     find_tan_y = lambda x,slope,intercept: (slope * x) + intercept 
     
-    tan_len = 6
+    tan_len = 4
     pt_scalars = range(-tan_len//2,tan_len//2+1)
     
     tan_xs = [x+pt_scalars for x in [x for x in x_vals]]
     tan_ys = []
     
     for slope,y_int,x in zip(tan_slopes, y_ints, tan_xs):
-        tan_ys.append(np.dstack((x,find_tan_y(x, slope, y_int))).tolist())
+        tan_ys.append(np.column_stack((x,find_tan_y(x, slope, y_int))))
 
     tan_ys = np.array(tan_ys)
     
@@ -43,8 +43,8 @@ def calc_tangent(splinerep_tck:tuple, u, x_vals, y_vals):
 
 def main():
     centreline = np.loadtxt(r'src\centreline_gen\output\centre_line.csv', delimiter = ',')
-    x = centreline[:100:,0]
-    y = centreline[:100:,1]
+    x = centreline[::,0]
+    y = centreline[::,1]
 
     spline_tck, u = create_splines(centreline)
     spline_x, spline_y = interpolate.splev(u, spline_tck)
@@ -54,11 +54,13 @@ def main():
     # plot the result
     fig, ax = plt.subplots(1, 1)
     ax.plot(x, y, 'or', label = 'data')
-    ax.plot(spline_x, spline_y, '-b', label = 'spline')
+    ax.plot(spline_x, spline_y, '-k', label = 'spline',  linewidth=1.5)
     
     colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
-    tans = LineCollection(tangents_y, colors=colors, linestyle='solid')
-
+    to_plot = [[tangents_y[i]] for i in range(len(tangents_y))]
+    
+    tans = LineCollection(tangents_y, colors=colors, linestyle='solid', linewidth=0.5)
+    ax.add_collection(tans)
 
 
     plt.legend()
