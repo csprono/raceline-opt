7bf0a376253ac6636c2bfa7312c92df5a6714718
Adam Keating <adamkeating@xtra.co.nz>
2023-09-25 20:57:52 +1300

    FSAE - cleaned up left/right lists

--- fsae.py
+++ fsae.py
@@ -1,17 +1,18 @@
 import cv2 as cv
 import numpy as np
 
-def adjust_bodyfit(occ_grid : np.array) -> np.array:
+def adjust_bodyfit(occ_grid : np.ndarray) -> np.ndarray:
     '''
         Inputs: 
         occ_grid = Occupancy grid with interpolated boundary
         resolution = distance per pixel
 
         Output:
-        - Occupancy grid that depicts drivable area and accounts for width of
-          the car
+        - shrunken_track = Occupancy grid that depicts drivable area and accounts 
+          for width of the car
     '''
-    shrinkage_amount = 15
+    
+    shrinkage_amount = 15 #need to account for resolution to check how many pixels to shrink by
     kernel_size = (shrinkage_amount // 2, shrinkage_amount // 2)
     kernel = np.ones(kernel_size, np.uint8)
 
@@ -20,7 +21,7 @@ def adjust_bodyfit(occ_grid : np.array) -> np.array:
     return shrunken_track
 
 
-def separate_l_r(occ_grid : np.array):
+def separate_l_r(occ_grid : np.ndarray):
     '''
         Inputs:
         occ_grid = Eroded occupancy grid 
@@ -34,24 +35,31 @@ def separate_l_r(occ_grid : np.array):
 
     new = cv.cvtColor(occ_grid, cv.COLOR_GRAY2BGR)
 
-    left, right = [], []
+    left, right = [],[]
 
     for i in range(len(contours)):
         # Draw the contour
         if hierarchy[0][i][3] == -1:
             # Outer contour, draw in red
-            cv.drawContours(new, contours, i, (0, 0, 255), 2)
+            cv.drawContours(new, contours, i, (0, 0, 255), 1)
 
             # Outer contour, store its points
             right.append(contours[i])
         else:
             # Inner contour, draw in blue
-            cv.drawContours(new, contours, i, (255, 0, 0), 2)
+            cv.drawContours(new, contours, i, (255, 0, 0), 1)
 
             # Inner contour, store its points
             left.append(contours[i])
 
     cv.imshow('contours', new)
+    
+    #clean boundary arrays
+    left = [inner.flatten() for inner in np.vstack(left)]
+    right = [inner.flatten() for inner in np.vstack(right)]
+
+    left, right = np.array(left), np.array(right)
+
     return left, right
 
 src = cv.imread('track.png')
